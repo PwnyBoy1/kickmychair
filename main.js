1449afe88a53812248ea4d013c6fd3aeebc09a40
PwnyBoy1 <112739235+PwnyBoy1@users.noreply.github.com>
2022-09-03 00:11:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let img = document.querySelector('img');
+let btn1 = document.querySelector('#IB1');
+var x = 1
+function switching(){
+    if(x==1){
+        document.getElementById("noick").src = 'KickMyC/kick.jpg';
+        x++;
+    } else if (x==2) {
+        document.getElementById("nokick").src = 'KickMyC/no_kick.jpg';
+        x=1;
+    }
+    
+}
+var data=1;
+document.getElementById('counter').innerText=data;
+function increment(){
+    data=data+1;
+    document.getElementById('counter').innerText=data;
+}
+
+
