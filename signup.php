87d0ed4bba202846fc1c303daf9eed279b586483
pwnyboy1 <fredi.rivera@hotmail.com>
2023-12-15 02:59:30 -0500

    Add styling to sign up form. Fix font

--- signup.php
+++ signup.php
@@ -7,22 +7,38 @@ session_start();
     <link rel="stylesheet" href="Style.css" type="text/css">
 </head>
 <div class="signup-body">
-    <div class="form-page">
 
         <div class="form-container" style="border: 1px solid black;" >
             <h2>Sign Up</h2>
             <form class="signup-form" action="includes/signup.inc.php" method="post">
-                <input type="text" name="name" placeholder="Full name...">
-                <input type="text" name="email" placeholder="Email...">
-                <input type="text" name="uid" placeholder="Username...">
-                <input type="password" name="pwd" placeholder="Password...">
-                <input type="password" name="pwdRepeat" placeholder="Repeat Password...">
-                <button type="Submit" name="submit">Sign Up</button>
-                
-            </form>
-        </div>
-     </div>
+                <div class="input-container">
+                    <input type="text" name="name" id="name" required>
+                    <label class="placeholder">Full Name</label>
+                </div>
 
+                <div class="input-container">
+                    <input type="text" name="email" id="email" required>
+                    <label class="placeholder">Email</label>
+                </div>
+
+                <div class="input-container">
+                    <input type="text" name="uid" id="uid" required>
+                    <label class="placeholder">Username</label>
+                </div>
+
+                <div class="input-container">
+                    <input type="password" name="pwd" id="pwd" required>
+                    <label class="placeholder">Password</label>
+                </div>
+
+                <div class="input-container">
+                    <input type="password" name="pwdRepeat" id="pwdRepeat" required>
+                    <label class="placeholder">Repeat Password</label>
+                </div>
+
+                <button type="submit" name="submit">Sign Up</button>
+                </form>
+        </div>
 
 
 </div>
