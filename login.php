8cff16279ac9aaefff5184dab00485168343c06f
pwnyboy1 <fredi.rivera@hotmail.com>
2023-12-15 23:55:01 -0500

    Add some styling from sign up form to log in form

--- login.php
+++ login.php
@@ -6,8 +6,14 @@ session_start();
         <div class="login-container">
             <h2>Log in</h2>
             <form class="login-form" action="./includes/login.inc.php" method="post">
-                <input type="text" name="uid" placeholder="Username/Email...">
-                <input type="password" name="pwd" placeholder="Password...">
+                <div class="input-container">
+                    <input type="text" name="uid">
+                    <label class="placeholder">Username or Email</label>
+                </div>
+                <div class="input-container">
+                    <input type="password" name="pwd">
+                    <label class="placeholder">Password</label>
+                </div>
                 <button type="submit" name="submit">Log In</button>
                 
             </form>
