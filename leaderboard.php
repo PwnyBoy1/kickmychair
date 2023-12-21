8e475e7459419758b48a0f6903773b982805a1bc
pwnyboy1 <fredi.rivera@hotmail.com>
2023-12-20 23:57:18 -0500

    Start Leaderboard logic for obtain results from data base

--- leaderboard.php
+++ leaderboard.php
@@ -1,14 +1,18 @@
 <?php
     session_start();
     include_once 'header.php';
+    include_once './dbh.inc.php';
+
+
+    $scoresResultsQuery = "SELECT users_uid, users_score FROM users ORDER BY users_score";
+
+    $startPage = 0;
+
+    $rowsPerPage = 10;
+
+
+
+
+
 ?>
-
-<div>
-
-    <table></table> 
-
-
-
-
-</div>
 
