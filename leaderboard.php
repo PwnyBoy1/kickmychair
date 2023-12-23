958441acf9b0aa9d6a8ce968461b6976d44ed446
pwnyboy1 <fredi.rivera@hotmail.com>
2023-12-23 18:44:20 -0500

    Add early stage leaderboard

--- leaderboard.php
+++ leaderboard.php
@@ -1,18 +1,33 @@
 <?php
     session_start();
     include_once 'header.php';
-    include_once './dbh.inc.php';
-
-
-    $scoresResultsQuery = "SELECT users_uid, users_score FROM users ORDER BY users_score";
+    include_once './includes/dbh.inc.php';
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+?>
+<?php
+    $sql = "SELECT users_uid, users_score FROM users ORDER BY users_score";
+    $scoresResults = mysqli_query($conn, $sql);
+    if (!$scoresResults) {
+        die('Error: ' . mysqli_error($conn));
+    }
+    $resultCheck = mysqli_num_rows($scoresResults);
+    
+    if ($resultCheck > 0) {
+        echo '<div>';
+        while ($row = mysqli_fetch_assoc($scoresResults)){
+            echo $row['users_uid'] . ':' . $row['users_score'] . "<br>";
+        }
+        echo '</div>';
+    } else {
+        echo "No results found.";
+    };
 
     $startPage = 0;
-
+    
     $rowsPerPage = 10;
 
-
-
-
+    $rowsPerPage = 10;
+    
 
 ?>
-
