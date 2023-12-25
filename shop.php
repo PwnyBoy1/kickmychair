a6de71caf2cb526f297171dfe803f4a9bacb8d70
pwnyboy1 <fredi.rivera@hotmail.com>
2023-12-25 15:02:57 -0500

    Fix styling on shop images of under construction

--- shop.php
+++ shop.php
@@ -1,7 +1,9 @@
 <?php
     include_once 'header.php';
 ?>
-<div class="construction">
-    <img src="./KickMyC/workerdom.png" alt="">
-    <img src="./KickMyC/halt.png" alt="Halt this section is under construction">
+<div class="shop-content">
+    <div class="construction">
+        <img src="./KickMyC/workerdom.png" alt="Dom working">
+        <img src="./KickMyC/halt.png" alt="Halt this section is under construction">
+    </div>
 </div>
